f2e72f252017a5bd8cfd0a687c5e8edd541fcff0
ElisavetKanidou <elisavetkanidou@gmail.com>
2022-06-01 18:55:04 +0300

    exit

--- src/Articles.java
+++ src/Articles.java
@@ -53,7 +53,6 @@ public class Articles extends JFrame {
 	 * Create the frame.
 	 */
 	public Articles() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 973, 1000);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
