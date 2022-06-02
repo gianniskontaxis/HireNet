8b6389017b49309d7479da4860980f7d307488db
NikosSakell <nikossakell02@gmail.com>
2022-06-02 14:52:02 +0300

    Profile design
    favorite number text field added

--- src/Profile.java
+++ src/Profile.java
@@ -16,6 +16,8 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.SwingConstants;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
 
 public class Profile extends JFrame {
 
@@ -28,6 +30,7 @@ public class Profile extends JFrame {
 	private ResultSet rs = null;
 	private String sql="";
 	private JTextField textFieldcomm;
+	private JTextField textField_2;
 	/*HashMap<Integer,List<String>> seminars = new HashMap<Integer,List<String>>();*/
 	/*public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -45,29 +48,71 @@ public class Profile extends JFrame {
 	
 	
 	public Profile(int i) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Profile.class.getResource("/Images/logo_icon25x25.png")));
+		setResizable(false);
 		this.i=i;
 
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 650, 650);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JLabel lblFavoriteNumber = new JLabel("Favorite number");
+		lblFavoriteNumber.setForeground(Color.WHITE);
+		lblFavoriteNumber.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblFavoriteNumber.setBounds(232, 293, 136, 22);
+		contentPane.add(lblFavoriteNumber);
+		
+		textField_2 = new JTextField();
+		textField_2.setText((String) null);
+		textField_2.setColumns(10);
+		textField_2.setBounds(232, 325, 163, 32);
+		contentPane.add(textField_2);
+		
+		JLabel Photo_label = new JLabel("");
+		Photo_label.setIcon(new ImageIcon(Profile.class.getResource("/Images/profilephoto.png")));
+		Photo_label.setFont(new Font("Tahoma", Font.BOLD, 13));
+		Photo_label.setForeground(Color.WHITE);
+		Photo_label.setBackground(Color.WHITE);
+		Photo_label.setHorizontalAlignment(SwingConstants.CENTER);
+		Photo_label.setBounds(36, 71, 126, 126);
+		contentPane.add(Photo_label);
+		
+		JLabel lblNewLabel_5 = new JLabel("");
+		lblNewLabel_5.setIcon(new ImageIcon(Profile.class.getResource("/Images/background1.png")));
+		lblNewLabel_5.setBounds(25, 62, 147, 528);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_4 = new JLabel("Edit Profile");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 18));
+		lblNewLabel_4.setForeground(Color.WHITE);
+		lblNewLabel_4.setBounds(274, 12, 121, 32);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_3 = new JLabel("");
+		lblNewLabel_3.setIcon(new ImageIcon(Profile.class.getResource("/Images/edit_icon.png")));
+		lblNewLabel_3.setBounds(232, 12, 32, 32);
+		contentPane.add(lblNewLabel_3);
+		
 		textField = new JTextField();	
 		
-		textField.setBounds(272, 90, 151, 19);
+		textField.setBounds(232, 103, 359, 32);
 		contentPane.add(textField);
 		textField.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("UserName");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblNewLabel.setBounds(153, 90, 72, 16);
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setBackground(new Color(255, 255, 255));
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblNewLabel.setBounds(232, 71, 121, 22);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblEmail = new JLabel("Email");
-		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblEmail.setBounds(153, 116, 47, 16);
+		lblEmail.setForeground(new Color(255, 255, 255));
+		lblEmail.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblEmail.setBounds(232, 145, 57, 22);
 		contentPane.add(lblEmail);
 		
 		textField_1 = new JTextField();
@@ -89,14 +134,15 @@ public class Profile extends JFrame {
 		
 		
 		JButton Delete=new JButton("Delete account");
-		Delete.setBackground(new Color(47, 79, 79));
+		Delete.setIcon(new ImageIcon(Profile.class.getResource("/Images/delete_icon.png")));
+		Delete.setBackground(new Color(0, 0, 0));
 		Delete.setForeground(new Color(255, 255, 255));
 		Delete.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
-		Delete.setBounds(293, 22, 121, 19);
+		Delete.setBounds(459, 558, 151, 32);
 		contentPane.add(Delete);
 		
 		textFieldcomm = new JTextField();
-		textFieldcomm.setBounds(272, 148, 96, 19);
+		textFieldcomm.setBounds(232, 251, 163, 32);
 		contentPane.add(textFieldcomm);
 		textFieldcomm.setColumns(10);
 		Delete.addActionListener(new ActionListener() {
@@ -105,7 +151,7 @@ public class Profile extends JFrame {
 			} });
 		
 		textField_1.setColumns(10);
-		textField_1.setBounds(272, 119, 151, 19);
+		textField_1.setBounds(232, 177, 359, 32);
         sql = "select * from users where id = '"+i+"'";
         try {
     		conn = DBConnection.ConnDB();
@@ -159,13 +205,10 @@ public class Profile extends JFrame {
 			
 		contentPane.add(textField_1);
 		
-		JLabel lblNewLabel_1 = new JLabel("Photograph");
-		lblNewLabel_1.setBounds(20, 90, 106, 135);
-		contentPane.add(lblNewLabel_1);
-		
-		JLabel lblCommunication = new JLabel("Communication");
-		lblCommunication.setFont(new Font("Tahoma", Font.PLAIN, 13));
-		lblCommunication.setBounds(153, 148, 106, 16);
+		JLabel lblCommunication = new JLabel("Phone number");
+		lblCommunication.setForeground(new Color(255, 255, 255));
+		lblCommunication.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblCommunication.setBounds(232, 219, 136, 22);
 		contentPane.add(lblCommunication);
 		
 		
@@ -196,11 +239,15 @@ public class Profile extends JFrame {
 			} });
 		*/
 		
-		JButton btnBack = new JButton("Back");
+		JButton btnBack = new JButton("");
+		btnBack.setIcon(new ImageIcon(Profile.class.getResource("/Images/back_icon.png")));
 		btnBack.setForeground(Color.WHITE);
 		btnBack.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
 		btnBack.setBackground(new Color(47, 79, 79));
-		btnBack.setBounds(20, 234, 78, 19);
+		btnBack.setBounds(10, 12, 32, 32);
+		btnBack.setOpaque(false);
+		btnBack.setContentAreaFilled(false);
+		btnBack.setBorderPainted(false);
 		contentPane.add(btnBack);
 		
 		btnBack.addActionListener(new ActionListener() {
@@ -211,11 +258,17 @@ public class Profile extends JFrame {
 		
 
 		JButton btnSave = new JButton("Save");
+		btnSave.setIcon(new ImageIcon(Profile.class.getResource("/Images/save_icon.png")));
 		btnSave.setForeground(Color.WHITE);
 		btnSave.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 11));
-		btnSave.setBackground(new Color(47, 79, 79));
-		btnSave.setBounds(159, 233, 78, 19);
+		btnSave.setBackground(new Color(0, 0, 0));
+		btnSave.setBounds(337, 558, 112, 32);
 		contentPane.add(btnSave);
+		
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon(Profile.class.getResource("/Images/profile_backgrnd.png")));
+		lblNewLabel_2.setBounds(0, 0, 636, 613);
+		contentPane.add(lblNewLabel_2);
 		btnSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				sql = "update users set username = '"+textField.getText()+"' , email= '"+textField_1.getText()+"' , phone = '"+textFieldcomm.getText()+"'   where id = '"+i+"'";
