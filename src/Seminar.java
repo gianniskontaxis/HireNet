42adf3560de6e0667cb7e30856b7d1d1cbffde3e
champos4 <babissemel@hotmail.com>
2022-06-09 18:21:00 +0300

    resize seminar/articles

--- src/Seminar.java
+++ src/Seminar.java
@@ -63,7 +63,7 @@ public class Seminar extends JFrame {
 		
 		setTitle("Seminars");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 1127, 720);
+		setBounds(100, 60, 1127, 720);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
