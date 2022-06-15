2c02c4f5617808efb99615aeaa50b922bbf1b479
ElisavetKanidou <elisavetkanidou@gmail.com>
2022-06-15 13:43:10 +0300

    new Accounts

--- src/Main.java
+++ src/Main.java
@@ -8,3 +8,14 @@ public class Main {
 }
 	
 
+/*
+  username, password, 
+  ics21095, ics21095Kanidou
+  ics21105, ics21105Kontaxis
+  Matina,   Matina123
+  ics21069, ics21069Abatzis
+  ics21016, ics21016Kotzampasi
+  ics21096, ics21096Sakellaris
+  ics21022, ics21022Gialagkolidou
+ */
+ 
